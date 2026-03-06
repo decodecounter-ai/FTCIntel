4f9f8dd3444fcb8f34279c5fb775162072ef86d2
decodecounter-ai <decodero2counter@gmail.com>
2026-03-06 19:30:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxHP2B72hfCry8SEtHikPk-SN1lO4Ax_RmDKq7x4WHiLmVv32L7Zp2WUv1U76M4x4cHRA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzgC6Q3K3PxYinuEtTucKQPUJWDHN_nEAOf8Vru9zxUcvXFSmj6cQCDMO0zh5hdfF4Wog/exec";
